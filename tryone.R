03ef3fc5327eedb46405209ab538a34e99ef5b4d
mrchypark <mrchypark@gmail.com>
2014-11-14 23:37:07 +0900

    폴더에서 파일 불러오기 완료 문제는 인코딩을 utf-8로 바꿔야 하는데 고쳐야 할 문제

--- tryone.R
+++ tryone.R
@@ -1,2 +1,9 @@
 movieList<-list.files('./data/movie')
-NumMovieList<-length(movieList)
+numMovieList<-length(movieList)
+movieNames<-gsub(".txt","",movieList)
+
+for (i in 1:numMovieList){
+	assign(eval(parse(text=paste0('movieNames[',i,']'))),eval(parse(text=paste0('readLines("./data/movie/',movieList[i],'")'))))
+	assign(Encoding(eval(parse(text=paste0('movieNames[',i,']')))),"UTF-8")
+}
+
