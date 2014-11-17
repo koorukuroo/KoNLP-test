0aaacdb1b8396d7d648b247f2bf14fdf3f58fd95
mrchypark <mrchypark@gmail.com>
2014-11-17 18:04:20 +0900

    iconv 로 해결해볼 수 있을지도

--- tryone.R
+++ tryone.R
@@ -4,6 +4,7 @@ movieNames<-gsub(".txt","",movieList)
 
 for (i in 1:numMovieList){
 	assign(eval(parse(text=paste0('movieNames[',i,']'))),eval(parse(text=paste0('readLines("./data/movie/',movieList[i],'")'))))
-	assign(Encoding(eval(parse(text=paste0('movieNames[',i,']')))),"UTF-8")
+	assign(Encoding(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']'))))))),"UTF-8")
 }
 
+head(iconv(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']')))))),from="CP1252",to="UTF-8"),10)
