6afa10e5e90db74721af25f69b95bed6d59e7e7e
mrchypark <mrchypark@gmail.com>
2014-11-17 20:00:22 +0900

    설명 추가

--- tryone.R
+++ tryone.R
@@ -1,7 +1,7 @@
-movieList<-list.files('./data/movie')
-numMovieList<-length(movieList)
-movieNames<-gsub(".txt","",movieList)
-
+movieList<-list.files('./data/movie')  ##고쳐야 겠지만 파일을 불러올 폴더에서 txt 파일 이름을 불러옵니다.
+numMovieList<-length(movieList)        ##불러와야할 txt 파일의 갯수를 확인합니다.
+movieNames<-gsub(".txt","",movieList)  ##파일이름을 변수로 사용하기 위해 .txt를 뗴어냅니다.
+## 포문은 파일 이름인 놈을 변수로 txt를 readLines 명령으로 불러와 list로 저장한다.
 for (i in 1:numMovieList){
 	assign(eval(parse(text=paste0('movieNames[',i,']'))),eval(parse(text=paste0('readLines("./data/movie/',movieList[i],'")'))))
 	assign(eval(parse(text=paste0('movieNames[',i,']'))),iconv(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']')))))), "UTF-8", "CP949"))
