b6f793b1e44bbd91c46d68a3f2677cf25df0ad4e
mrchypark <mrchypark@gmail.com>
2014-11-17 19:50:22 +0900

    utf-8 문제 해결

--- tryone.R
+++ tryone.R
@@ -4,7 +4,5 @@ movieNames<-gsub(".txt","",movieList)
 
 for (i in 1:numMovieList){
 	assign(eval(parse(text=paste0('movieNames[',i,']'))),eval(parse(text=paste0('readLines("./data/movie/',movieList[i],'")'))))
-	assign(Encoding(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']'))))))),"UTF-8")
+	assign(eval(parse(text=paste0('movieNames[',i,']'))),iconv(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']')))))), "UTF-8", "CP949"))
 }
-
-head(iconv(eval(parse(text=paste0(eval(parse(text=paste0('movieNames[',i,']')))))),from="CP1252",to="UTF-8"),10)
